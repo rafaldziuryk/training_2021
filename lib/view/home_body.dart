739465f4f1a2068864ff4013f414f93098aa51ef
Rafał Dziuryk <rafal.dziuryk@siroccomobile.com>
2021-07-15 23:17:56 +0200

    chaange page name - import fix

--- lib/view/home_body.dart
+++ lib/view/home_body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/view/passenger_page.dart';
+import 'package:flutter_training/view/pokemons_page.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody(this.selectedIndex, {Key? key}) : super(key: key);
